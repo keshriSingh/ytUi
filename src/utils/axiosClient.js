98c97556d2c8f25d039b4bfc5efb4473908ab651
keshri singh <keshrisingh52@gmail.com>
2025-11-24 18:34:34 +0530

    Added production build and configuration

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL:"http://localhost:3333",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3333",
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
